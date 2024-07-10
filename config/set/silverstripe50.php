927e47c6922e40f45e822cefa40c1c9c4090f3da
Cameron Bryers <12287346+Cambis@users.noreply.github.com>
2024-07-10 20:38:31 +1200

    :bug: fix: include missing service in some sets

--- config/set/silverstripe50.php
+++ config/set/silverstripe50.php
@@ -2,8 +2,14 @@
 
 declare(strict_types=1);
 
+use Cambis\SilverstripeRector\Silverstripe413\TypeResolver\ConfigurationPropertyTypeResolver;
+use Cambis\SilverstripeRector\TypeResolver\Contract\ConfigurationPropertyTypeResolverInterface;
 use Rector\Config\RectorConfig;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/../config.php');
+    $rectorConfig->singleton(
+        ConfigurationPropertyTypeResolverInterface::class,
+        ConfigurationPropertyTypeResolver::class
+    );
 };
