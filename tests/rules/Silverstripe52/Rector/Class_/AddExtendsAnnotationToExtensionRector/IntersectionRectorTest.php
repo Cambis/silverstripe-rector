f0f32716b18f8557a08404121ae8114022836afe
Cameron Bryers <12287346+Cambis@users.noreply.github.com>
2024-08-27 15:33:13 +1200

    :sparkles: feat: update AddExtendsAnnotationToExtensionRectorTest
    - This test can now return the correct types without mocking
    - Add a configuration property value object for testing

--- tests/rules/Silverstripe52/Rector/Class_/AddExtendsAnnotationToExtensionRector/IntersectionRectorTest.php
+++ tests/rules/Silverstripe52/Rector/Class_/AddExtendsAnnotationToExtensionRector/IntersectionRectorTest.php
@@ -4,23 +4,62 @@ declare(strict_types=1);
 
 namespace Cambis\SilverstripeRector\Tests\Silverstripe52\Rector\Class_\AddExtendsAnnotationToExtensionRector;
 
+use Cambis\SilverstripeRector\Testing\PHPUnit\ValueObject\ConfigurationProperty;
+use Cambis\SilverstripeRector\Tests\Silverstripe52\Rector\Class_\AddExtendsAnnotationToExtensionRector\FixtureIntersection\HasManyOwners;
+use Cambis\SilverstripeRector\Tests\Silverstripe52\Rector\Class_\AddExtendsAnnotationToExtensionRector\FixtureIntersection\HasManyOwnersComplete;
+use Cambis\SilverstripeRector\Tests\Silverstripe52\Rector\Class_\AddExtendsAnnotationToExtensionRector\FixtureIntersection\HasManyOwnersInComplete;
+use Cambis\SilverstripeRector\Tests\Silverstripe52\Rector\Class_\AddExtendsAnnotationToExtensionRector\Source\OwnerMockOne;
+use Cambis\SilverstripeRector\Tests\Silverstripe52\Rector\Class_\AddExtendsAnnotationToExtensionRector\Source\OwnerMockTwo;
+use Cambis\SilverstripeRector\ValueObject\SilverstripeConstants;
 use Iterator;
 use Override;
 use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+use SilverStripe\Core\Config\Config;
 
 final class IntersectionRectorTest extends AbstractRectorTestCase
 {
     /**
      * @dataProvider provideData
+     * @param ConfigurationProperty[] $configurationProperties
      */
-    public function test(string $filePath): void
+    public function test(array $configurationProperties, string $filePath): void
     {
+        foreach ($configurationProperties as $configurationProperty) {
+            Config::modify()->set(
+                $configurationProperty->className,
+                $configurationProperty->propertyName,
+                $configurationProperty->value,
+            );
+        }
+
         $this->doTestFile($filePath);
     }
 
     public static function provideData(): Iterator
     {
-        return self::yieldFilesFromDirectory(__DIR__ . '/FixtureIntersection');
+        yield [
+            [
+                new ConfigurationProperty(OwnerMockOne::class, SilverstripeConstants::PROPERTY_EXTENSIONS, [HasManyOwners::class]),
+                new ConfigurationProperty(OwnerMockTwo::class, SilverstripeConstants::PROPERTY_EXTENSIONS, [HasManyOwners::class]),
+            ],
+            __DIR__ . '/FixtureIntersection/has_many_owners.php.inc',
+        ];
+
+        yield [
+            [
+                new ConfigurationProperty(OwnerMockOne::class, SilverstripeConstants::PROPERTY_EXTENSIONS, [HasManyOwnersComplete::class]),
+                new ConfigurationProperty(OwnerMockTwo::class, SilverstripeConstants::PROPERTY_EXTENSIONS, [HasManyOwnersComplete::class]),
+            ],
+            __DIR__ . '/FixtureIntersection/has_many_owners_complete.php.inc',
+        ];
+
+        yield [
+            [
+                new ConfigurationProperty(OwnerMockOne::class, SilverstripeConstants::PROPERTY_EXTENSIONS, [HasManyOwnersInComplete::class]),
+                new ConfigurationProperty(OwnerMockTwo::class, SilverstripeConstants::PROPERTY_EXTENSIONS, [HasManyOwnersInComplete::class]),
+            ],
+            __DIR__ . '/FixtureIntersection/has_many_owners_incomplete.php.inc',
+        ];
     }
 
     #[Override]
