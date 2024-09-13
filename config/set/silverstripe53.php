bbb8922119433f06bf4eccc0c4657f40e78f5932
Cameron Bryers <12287346+Cambis@users.noreply.github.com>
2024-09-14 11:19:52 +1200

    :sparkles: feat: add API class renames from changelog

--- config/set/silverstripe53.php
+++ config/set/silverstripe53.php
@@ -5,11 +5,28 @@ declare(strict_types=1);
 use Cambis\SilverstripeRector\Silverstripe52\TypeResolver\ConfigurationPropertyTypeResolver;
 use Cambis\SilverstripeRector\TypeResolver\Contract\ConfigurationPropertyTypeResolverInterface;
 use Rector\Config\RectorConfig;
+use Rector\Renaming\Rector\Name\RenameClassRector;
 
+// See: https://docs.silverstripe.org/en/5/changelogs/5.3.0/
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/../config.php');
+
     $rectorConfig->singleton(
         ConfigurationPropertyTypeResolverInterface::class,
         ConfigurationPropertyTypeResolver::class
+    );
+
+    $rectorConfig->ruleWithConfiguration(
+        RenameClassRector::class,
+        [
+            // https://github.com/silverstripe/silverstripe-framework/pull/11344
+            'SilverStripe\Control\Util\IpUtils' => 'Symfony\Component\HttpFoundation\IpUtils',
+            // https://github.com/silverstripe/silverstripe-admin/pull/1812
+            'SilverStripe\Admin\LeftAndMainExtension' => 'SilverStripe\Core\Extension',
+            // https://github.com/silverstripe/silverstripe-cms/pull/2991
+            'SilverStripe\CMS\Model\SiteTreeExtension' => 'SilverStripe\Core\Extension',
+            // https://github.com/silverstripe/silverstripe-framework/pull/11347
+            'SilverStripe\ORM\DataExtension' => 'SilverStripe\Core\Extension',
+        ]
     );
 };
