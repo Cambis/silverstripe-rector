353242abe0e30037ddab165ea4b8c44dfdb30bd0
Cameron Bryers <12287346+Cambis@users.noreply.github.com>
2024-09-15 12:26:14 +1200

    :bug: fix: update silverstripe 53 level setlist
    - Ensure that the correct type resolver is registered

--- config/set/level/up-to-silverstripe53.php
+++ config/set/level/up-to-silverstripe53.php
@@ -4,8 +4,15 @@ declare(strict_types=1);
 
 use Cambis\SilverstripeRector\Set\ValueObject\SilverstripeLevelSetList;
 use Cambis\SilverstripeRector\Set\ValueObject\SilverstripeSetList;
+use Cambis\SilverstripeRector\Silverstripe53\TypeResolver\ConfigurationPropertyTypeResolver;
+use Cambis\SilverstripeRector\TypeResolver\Contract\ConfigurationPropertyTypeResolverInterface;
 use Rector\Config\RectorConfig;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->sets([SilverstripeSetList::SILVERSTRIPE_53, SilverstripeLevelSetList::UP_TO_SILVERSTRIPE_52]);
+
+    $rectorConfig->singleton(
+        ConfigurationPropertyTypeResolverInterface::class,
+        ConfigurationPropertyTypeResolver::class
+    );
 };
