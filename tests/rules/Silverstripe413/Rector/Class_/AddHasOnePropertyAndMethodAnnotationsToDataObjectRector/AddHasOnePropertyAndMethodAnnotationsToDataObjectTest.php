160a5abb9f97ad6582979ec5da94548dbbe485c3
Cameron Bryers <12287346+Cambis@users.noreply.github.com>
2025-01-07 18:26:16 +1300

    :sparkles: chore: clean up

--- tests/rules/Silverstripe413/Rector/Class_/AddHasOnePropertyAndMethodAnnotationsToDataObjectRector/AddHasOnePropertyAndMethodAnnotationsToDataObjectTest.php
+++ tests/rules/Silverstripe413/Rector/Class_/AddHasOnePropertyAndMethodAnnotationsToDataObjectRector/AddHasOnePropertyAndMethodAnnotationsToDataObjectTest.php
@@ -4,33 +4,12 @@ declare(strict_types=1);
 
 namespace Cambis\SilverstripeRector\Tests\Silverstripe413\Rector\Class_\AddHasOnePropertyAndMethodAnnotationsToDataObjectRector;
 
-use Cambis\SilverstripeRector\Tests\Silverstripe413\Rector\Class_\AddHasOnePropertyAndMethodAnnotationsToDataObjectRector\Fixture\HasOne;
-use Cambis\SilverstripeRector\Tests\Silverstripe413\Rector\Class_\AddHasOnePropertyAndMethodAnnotationsToDataObjectRector\Source\RelationMock;
-use Cambis\SilverstripeRector\ValueObject\SilverstripeConstants;
 use Iterator;
 use Override;
 use Rector\Testing\PHPUnit\AbstractRectorTestCase;
-use SilverStripe\Core\Config\Config;
 
 final class AddHasOnePropertyAndMethodAnnotationsToDataObjectTest extends AbstractRectorTestCase
 {
-    /**
-     * Load properties via {@see SilverStripe\Core\Config\Config::modify()} in order for this to work in this testing environment.
-     */
-    #[Override]
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        Config::modify()->merge(
-            HasOne::class,
-            SilverstripeConstants::PROPERTY_HAS_ONE,
-            [
-                'HasOneRelationship' => RelationMock::class,
-            ]
-        );
-    }
-
     /**
      * @dataProvider provideData
      */
--- tests/rules/Silverstripe413/Rector/Class_/AddManyManyMethodAnnotationsToDataObjectRector/AddManyManyMethodAnnotationsToDataObjectRectorTest.php
+++ tests/rules/Silverstripe413/Rector/Class_/AddManyManyMethodAnnotationsToDataObjectRector/AddManyManyMethodAnnotationsToDataObjectRectorTest.php
@@ -4,50 +4,12 @@ declare(strict_types=1);
 
 namespace Cambis\SilverstripeRector\Tests\Silverstripe413\Rector\Class_\AddManyManyMethodAnnotationsToDataObjectRector;
 
-use Cambis\SilverstripeRector\Tests\Silverstripe413\Rector\Class_\AddManyManyMethodAnnotationsToDataObjectRector\Fixture\ManyManyThrough;
-use Cambis\SilverstripeRector\Tests\Silverstripe413\Rector\Class_\AddManyManyMethodAnnotationsToDataObjectRector\Fixture\ManyManyThroughComplete;
-use Cambis\SilverstripeRector\Tests\Silverstripe413\Rector\Class_\AddManyManyMethodAnnotationsToDataObjectRector\Source\RelationMock;
-use Cambis\SilverstripeRector\ValueObject\SilverstripeConstants;
 use Iterator;
 use Override;
 use Rector\Testing\PHPUnit\AbstractRectorTestCase;
-use SilverStripe\Core\Config\Config;
 
 final class AddManyManyMethodAnnotationsToDataObjectRectorTest extends AbstractRectorTestCase
 {
-    /**
-     * Load properties via {@see SilverStripe\Core\Config\Config::modify()} in order for this to work in this testing environment.
-     */
-    #[Override]
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        // Config::modify()->merge(
-        //     ManyManyThrough::class,
-        //     SilverstripeConstants::PROPERTY_MANY_MANY,
-        //     [
-        //         'ManyManyThroughRelationship' => [
-        //             'through' => RelationMock::class,
-        //             'from' => '',
-        //             'to' => '',
-        //         ],
-        //     ]
-        // );
-
-        // Config::modify()->merge(
-        //     ManyManyThroughComplete::class,
-        //     SilverstripeConstants::PROPERTY_MANY_MANY,
-        //     [
-        //         'ManyManyThroughRelationship' => [
-        //             'through' => RelationMock::class,
-        //             'from' => '',
-        //             'to' => '',
-        //         ],
-        //     ]
-        // );
-    }
-
     /**
      * @dataProvider provideData
      */
