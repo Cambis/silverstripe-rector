3b5b55f9a9ed6ca4192472fb55ff7e98d5538faa
Cameron Bryers <12287346+Cambis@users.noreply.github.com>
2025-02-04 18:10:53 +1300

    :heavy_minus_sign: feat: remove webmozart/assert
    - This wasn't really needed, remove it in order to slim the dependency tree

--- src/PhpDoc/AnnotationComparator.php
+++ src/PhpDoc/AnnotationComparator.php
@@ -7,18 +7,16 @@ namespace Cambis\SilverstripeRector\PhpDoc;
 use Cambis\SilverstripeRector\PhpDoc\Contract\AnnotationComparatorInterface;
 use PhpParser\Node;
 use PHPStan\PhpDocParser\Ast\PhpDoc\PhpDocTagValueNode;
-use Webmozart\Assert\Assert;
 use function is_a;
 
 final readonly class AnnotationComparator
 {
     public function __construct(
         /**
-         * @var AnnotationComparatorInterface<PhpDocTagValueNode>[]
+         * @var list<AnnotationComparatorInterface<PhpDocTagValueNode>>
          */
         private array $annotationComparators
     ) {
-        Assert::notEmpty($annotationComparators);
     }
 
     public function areTagValueNodeNamesEqual(PhpDocTagValueNode $originalNode, PhpDocTagValueNode $newNode, Node $node): bool
