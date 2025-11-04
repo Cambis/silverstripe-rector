483035b4c45f599bb95b1f333cffc549ec01d6e0
Cameron Bryers <12287346+Cambis@users.noreply.github.com>
2025-11-05 11:58:03 +1300

    :sparkles: feat: update gorriecoe link migration set (#53)

--- config/set/gorriecoe-link-to-silverstripe-linkfield.php
+++ config/set/gorriecoe-link-to-silverstripe-linkfield.php
@@ -3,13 +3,25 @@
 use Cambis\SilverstripeRector\LinkField\Rector\Class_\GorriecoeLinkToSilverstripeLinkRector;
 use Cambis\SilverstripeRector\LinkField\Rector\StaticCall\GorriecoeLinkFieldToSilverstripeLinkFieldRector;
 use Rector\Config\RectorConfig;
+use Rector\Renaming\Rector\MethodCall\RenameMethodRector;
 use Rector\Renaming\Rector\Name\RenameClassRector;
+use Rector\Renaming\Rector\PropertyFetch\RenamePropertyRector;
+use Rector\Renaming\ValueObject\MethodCallRename;
+use Rector\Renaming\ValueObject\RenameProperty;
 
 return RectorConfig::configure()
     ->withRules([
         GorriecoeLinkToSilverstripeLinkRector::class,
         GorriecoeLinkFieldToSilverstripeLinkFieldRector::class,
     ])
+    ->withConfiguredRule(
+        RenamePropertyRector::class,
+        [new RenameProperty('gorriecoe\Link\Models\Link', 'LinkURL', 'URL')]
+    )
+    ->withConfiguredRule(
+        RenameMethodRector::class,
+        [new MethodCallRename('gorriecoe\Link\Models\Link', 'getLinkURL', 'getURL')]
+    )
     ->withConfiguredRule(
         RenameClassRector::class,
         [
