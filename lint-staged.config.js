ec721045323320904ed5f296d9c06cc0c647e650
Cameron Bryers <12287346+Cambis@users.noreply.github.com>
2026-03-09 09:00:23 +1300

    :rotating_light: chore: address linting issue

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,12 +1,14 @@
-/** @type {import('lint-staged').Config} */
-module.exports = {
+/**
+ * @type {import('lint-staged').Configuration}
+ */
+const config = {
   '*.php': (filenames) => [
     `php vendor/bin/parallel-lint src tests --colors --blame ${filenames.join(' ')}`,
     `php vendor/bin/rector process --dry-run --ansi ${filenames.join(' ')}`,
     `php vendor/bin/ecs check --fix --ansi ${filenames.join(' ')}`,
     'php vendor/bin/phpstan analyse --ansi --memory-limit=-1',
   ],
-  '!(e2e/*/composer.json)composer.json': [
-    'composer normalize --ansi',
-  ],
+  'composer.json': ['composer normalize --ansi'],
 };
+
+export default config;
